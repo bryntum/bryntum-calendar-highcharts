03249ecb435402f2798387cce5747d7736f15e2f
Arsalan Khattak <me@khattak.dev>
2026-03-03 17:47:01 +0500

    data table css

--- src/lib/HighchartsWidget.js
+++ src/lib/HighchartsWidget.js
@@ -29,7 +29,7 @@ export default class HighchartsWidget extends Widget {
     compose() {
         return {
             class    : 'b-chart',
-            style    : 'width:100%;height:100%;',
+            style    : 'width:100%;height:100%;overflow:auto;padding: 0px 10px;',
             children : {
                 chartElement : {
                     tag   : 'div',
